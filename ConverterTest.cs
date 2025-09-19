4828115d6b10ecfa484253f0cbfaa2e6c1858a11
Thomas Martinsen <thomas.martinsen@virinco.com>
2025-09-19 11:44:52 +0200

    Add project files.

---
+++ ConverterTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using Virinco.WATS.Interface;
+
+namespace SigmaQuest
+{
+    [TestClass]
+    public class ConverterTests : TDM
+    {
+
+        [TestMethod]
+        public void ConverterTest()
+        {
+            InitializeAPI(true);
+            string fn = @"Data\test.xml";
+            var converter = new GenericXMLConverter();
+            using (FileStream file = new FileStream(fn, FileMode.Open))
+            {
+                SetConversionSource(new FileInfo(fn), converter.ConverterParameters, null);
+                Report uut = converter.ImportReport(this, file);
+                Submit(uut);
+            }
+        }
+    }
+}
